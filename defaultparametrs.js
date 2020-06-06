a1a68013abf771875f71e4e2b32ac02afb8e6c69
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-06-06 12:10:30 -0700

    added default parameters

---
+++ defaultparametrs.js
@@ -0,0 +1,28 @@
+// Old
+ function multiply(a , b)
+ {
+   console.log(a * b);
+ }
+ multiply(5,6);
+  
+
+ function Multiply(){
+   var a = 1;
+   var b = 6;
+   console.log(a*b);
+
+ }
+ Multiply();
+
+ // ES6
+
+ const add = (x, y) => {
+   console.log(x+y);
+ }
+ add(3, 5);
+  
+ const Add = (X=5, Y=7) => {
+   console.log(X+Y);
+
+ }
+ Add();
