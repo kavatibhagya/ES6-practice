59cd61049d82d28fce6e14de31369008b1f74e71
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-06-06 15:36:00 -0700

    added for each

---
+++ foreach.js
@@ -0,0 +1,7 @@
+const shoppingList = ["eggs", "milk", "sugar","salt","banana"];
+shoppingList.forEach((product,index) => {
+   console.log(`this is the index ${index}  and this is the product ${product}`);
+  
+   
+})
+ 
